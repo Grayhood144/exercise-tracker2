15f796bb2169c270b47e4ea5c26a5c0c09861658
Grayhood114 <Graysonzrelak@gmail.com>
2025-02-25 13:59:18 -0500

    Fixed Git tracking issues and updated deployment setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Exercise Tracker</h1>
+      <p>Welcome to the exercise tracker app!</p>
+    </div>
+  );
+}
+
+export default App;
